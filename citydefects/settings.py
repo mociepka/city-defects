957623b7790161e4ae6f07932f7b292c46bc2fcb
Michał Ociepka <michal@ociepka.info>
2013-07-13 23:33:16 +0200

    Add defect model

--- citydefects/settings.py
+++ citydefects/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'defect',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -80,3 +82,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DEFAULT_LAT = '50.827978'
+DEFAULT_LNG = '15.525992'
--- defect/models.py
+++ defect/models.py
@@ -1,3 +1,16 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+
+class Defect(models.Model):
+
+    title = models.CharField(max_length=80)
+    slug = models.SlugField(max_length=80)
+    description = models.TextField()
+    street = models.CharField(max_length=255)
+    lat = models.FloatField('Latitude', default=settings.DEFAULT_LAT)
+    lng = models.FloatField('Longitude', default=settings.DEFAULT_LNG)
+    publicated = models.BooleanField()
+    user = models.ForeignKey(User)
