07a45282c9193a98f4d313f70c7a5c04331eb289
Michał Ociepka <michal@ociepka.info>
2013-07-14 00:35:33 +0200

    Add citydefects home view

--- citydefects/urls.py
+++ citydefects/urls.py
@@ -4,9 +4,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'citydefects.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += patterns('citydefects.views',
+    url(r'^$', 'home', name='home'),
 )
