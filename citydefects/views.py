87d288f8b7e46f6ac28efc5b6fd6cd5e271fd628
Michał Ociepka <michal@ociepka.info>
2013-07-14 11:24:23 +0200

    Use defect form

--- citydefects/views.py
+++ citydefects/views.py
@@ -1,9 +1,16 @@
-from django.template.response import TemplateResponse
+from defect.forms import DefectForm
 from django.conf import settings
+from django.shortcuts import redirect
+from django.template.response import TemplateResponse
 
 
 def home(request):
+    form = DefectForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('home')
     return TemplateResponse(request, 'home.html', {
         'map': {'lat': settings.DEFAULT_LAT, 'lng': settings.DEFAULT_LNG,
-                'zoom': 15}
+                'zoom': 15},
+        'form': form
     })
