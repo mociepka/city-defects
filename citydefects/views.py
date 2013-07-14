cc02a0001345bdfae9b6d9e9e2618cae3e879dc2
Michał Ociepka <michal@ociepka.info>
2013-07-14 02:00:08 +0200

    Add goole map

--- citydefects/views.py
+++ citydefects/views.py
@@ -1,5 +1,9 @@
 from django.template.response import TemplateResponse
+from django.conf import settings
 
 
 def home(request):
-    return TemplateResponse(request, 'home.html')
+    return TemplateResponse(request, 'home.html', {
+        'map': {'lat': settings.DEFAULT_LAT, 'lng': settings.DEFAULT_LNG,
+                'zoom': 15}
+    })
