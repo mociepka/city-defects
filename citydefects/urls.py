a7d2744dc5df33d19f71d18d310b148ebb2bfca5
Michał Ociepka <michal@ociepka.info>
2013-09-15 22:33:37 +0200

    Add media serving

--- citydefects/urls.py
+++ citydefects/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+from django.conf import settings
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -12,3 +13,11 @@ urlpatterns += patterns('citydefects.views',
     url(r'^api/streets/$', 'street_cache', name='streets'),
     url(r'^api/', include('defect.urls')),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('django.views.static',
+        url(r'^%s(?P<path>.*)$' % settings.MEDIA_URL[1:], 'serve', {
+            'document_root': settings.MEDIA_ROOT,
+            'show_indexes': True
+        }),
+    )
